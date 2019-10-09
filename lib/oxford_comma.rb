8e95b68b4c65d9c876e4bcc10c31d2b6747b413b
Victoria Hale <vic.hale74@gmail.com>
2019-10-09 22:12:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.size == 2 
   return array.join(" and ")
   elsif array.size >= 3
-  array[-1] = " and #{array[-1]}"
+  array[-1] = "and #{array[-1]}"
   return array.join(", ") 
   end
 end
